2c9b469e1c6de8ee97441067f8f4135c07a5f3b9
kkc <bushwick97@naver.com>
2025-01-22 19:59:34 +0900

    3rd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import "../styles/global.css";
 import Navigation from "../components/navigation";
 import {Metadata} from "next";
+
 
 export const metadata: Metadata = {
     title: {
         template: "%s | Next Movies",
         default: "Next Movies",
     },
-    description: 'The best movies on the best framework',
+    description: "The best movies on the best framework",
 };
 
 export default function RootLayout({
