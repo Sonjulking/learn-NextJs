6a854a840173aeca1b1982c6d5a36caab19b5738
kkc <bushwick97@naver.com>
2025-01-24 05:24:11 +0900

    params2

--- app/(movies)/movies/page.tsx
+++ app/(movies)/movies/page.tsx
@@ -1,8 +1,18 @@
+/*
 export default function Page({
     searchParams,
 }: {
     searchParams: { [key: string]: string | string[] | undefined }
 }) {
     const hello = searchParams.hell
+    return <div>ID: {hello}</div>;
+}*/
+interface PageProps {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Page({searchParams}: PageProps) {
+    const params = await searchParams;
+    const hello = params.hell;
     return <div>ID: {hello}</div>;
 }
